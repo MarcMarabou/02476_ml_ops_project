acf9504d82889b1c407b286d73dd8d4085f29897
Marc Bøg <marc@boeg.io>
2022-01-07 13:20:21 +0100

    download and extract flower data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,23 +1,52 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from zipfile import ZipFile
+from os import path, makedirs, system
+from tfrecord.torch.dataset import TFRecordDataset
+
+
+def extract_data(file_path: str) -> None:
+    """
+    Extracts data downloaded from kaggle
+
+    Args
+        file_path (str) : File to extract
+    """
+    print("Extracting data")
+    with ZipFile(file_path, "r") as zip:
+        zip.extractall(path.join("data", "raw", "flowers"))
+
+
+def download_data(download_path: str) -> None:
+    system(
+        "kaggle competitions download "
+        + "-c tpu-getting-started "
+        + f'-p "{download_path}"'
+    )
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("making final data set from raw data")
+
+    if not path.exists(path.join(input_filepath, "flowers")):
+        if not path.exists(path.join(input_filepath, "tpu-getting-started.zip")):
+            download_data(input_filepath)
+        extract_data(path.join(input_filepath, "tpu-getting-started.zip"))
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
