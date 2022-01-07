0aba77efb51c551dd0bfe43b2238fd0e817001d4
telkamp7 <kasper.telkamp@gmail.com>
2022-01-07 14:36:44 +0100

    changed parameters for the ViT model

--- models/ViT.py
+++ models/ViT.py
@@ -2,13 +2,13 @@ import torch.nn as nn
 import kornia.contrib as K
 
 class ViT(nn.Module):
-    def __init__(self, img_size, in_channels, targets):
+    def __init__(self):
         super().__init__()
         
         # We define the model
         self.layers = nn.Sequential(
-            K.VisionTransformer(image_size=img_size, in_channels=in_channels),
-            K.ClassificationHead(num_classes=targets)
+            K.VisionTransformer(),
+            K.ClassificationHead(num_classes=104)
         )
     
     def forward(self, x):
