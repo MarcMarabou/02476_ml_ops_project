33ed40f064d3ee49b5aaf371f9ae410d2b21ef99
Marc Bøg <marc@boeg.io>
2022-01-07 13:31:44 +0100

    make it work if data folder doesn't exist

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+import glob
 
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
@@ -30,7 +31,7 @@ def download_data(download_path: str) -> None:
 
 
 @click.command()
-@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("input_filepath", type=click.Path())
 @click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
     """Runs data processing scripts to turn raw data from (../raw) into
@@ -39,10 +40,13 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
 
-    if not path.exists(path.join(input_filepath, "flowers")):
-        if not path.exists(path.join(input_filepath, "tpu-getting-started.zip")):
+    flower_path = path.join(input_filepath, "flowers")
+    if not path.exists(flower_path):
+        makedirs(flower_path)
+        zip_path = path.join(input_filepath, "tpu-getting-started.zip")
+        if not path.exists(zip_path):
             download_data(input_filepath)
-        extract_data(path.join(input_filepath, "tpu-getting-started.zip"))
+        extract_data(zip_path)
 
 
 if __name__ == "__main__":
