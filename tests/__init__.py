21b6f528b6665af07667f8d68646aba397720a5d
Marc Bøg <s173905@student.dtu.dk>
2022-01-12 21:02:22 +0100

    data test

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+from os import path
+_TEST_ROOT = path.dirname(__file__)
+_PROJECT_ROOT = path.dirname(_TEST_ROOT)
+_PATH_DATA = path.join(_PROJECT_ROOT, "data")
---
+++ tests/test_data.py
@@ -0,0 +1,19 @@
+from src.data.FlowerDataset import FlowerDataset
+import pytest
+
+PATH = "data/processed/flowers"
+SIZE = "224x224"
+
+@pytest.mark.parametrize("split", ["train", "test"])
+def test_shape(split: str):
+    ds = FlowerDataset(PATH, SIZE, split)
+    assert len(ds) == (12753 if split == "train" else 7382)
+
+    h, w = SIZE.split("x")
+    for img, lbl in ds:
+        h = int(h); w = int(w)
+        assert img.shape == (3, h, w), "Shape mismatch"
+        if split == "test":
+            assert lbl == None, "Label is not None"
+        else:
+            assert lbl.nelement() == 1, "More than 1 label"
