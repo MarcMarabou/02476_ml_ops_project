3b15626b0b7842d71e2f566657439ceff7c43d41
jaco9012 <jacob.bahnsen.schmidt@gmail.com>
2022-01-13 12:34:53 +0100

    unit test for the model output

---
+++ tests/test_model.py
@@ -0,0 +1,13 @@
+import torch
+from models.ViT import ViT
+
+
+def test_predictions():
+    model = ViT()
+    model.train()
+    randImage = torch.rand([1, 3, 224, 224])
+
+    preds = model(randImage)
+    assert torch.all(1.0 >= preds) and torch.all(0.0 <= preds), "The model output either does not predict values between 0 and 1"
+    # needs to be rounded as the cpu fails to add stuff correctly on the 8-ish decimal place 
+    assert 1.0 == round(torch.sum(preds).item()), "The model predictions does not sum to 1"
