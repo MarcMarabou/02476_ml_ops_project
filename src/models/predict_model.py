1e792450ed59991841a8caefebde1b918adfdd28
telkamp7 <kasper.telkamp@gmail.com>
2022-01-19 11:34:50 +0100

    load model ckpt

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -9,27 +9,26 @@ import torch
 from pytorch_lightning import Trainer
 
 from src.data.FlowerDataset import FlowerDataset
-from src.models.task import get_args
 from src.models.ViT import ViT
-
+from src.models.task import get_args
 
 def main():
     # Training settings
     args = get_args()
 
     # Load the training data
-    test_set = FlowerDataset(args.data_path, "224x224", "test")
-    testloader = torch.utils.data.DataLoader(
-        test_set,
-        batch_size=len(test_set),
-        shuffle=True,
+    predict_set = FlowerDataset(args.data_path, "224x224", "test")
+    predictloader = torch.utils.data.DataLoader(
+        predict_set,
+        batch_size=args.batch_size,
+        shuffle=False,
         num_workers=args.num_workers,
     )
 
-    model = ViT(args=args)
-
-    trainer.fit(model, trainloader, valloader)
-
+    model = ViT.load_from_checkpoint(checkpoint_path=args.load_model_ckpt)
+    trainer = Trainer()
+    predictions = trainer.predict(model, predictloader)
+    print(predictions)
 
 if __name__ == "__main__":
     main()
