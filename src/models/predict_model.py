505fb6d17e5d0bc661847d1764c40b96892c7d53
telkamp7 <kasper.telkamp@gmail.com>
2022-01-20 10:03:29 +0100

    predict model

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -8,30 +8,13 @@ import gcsfs
 import matplotlib.pyplot as plt
 import torch
 
+from copy import deepcopy
+
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import Callback
 from src.data.FlowerDataset import FlowerDataset
 from src.models.task import get_args
 from src.models.ViT import ViT
-
-class DriftDetection(Callback):
-    def teardown(self, trainer, pl_module, stage):
-
-        drift_detector = torch.load(os.path.join('models/trained_models', self.hparams.load_timestamp, '*.pt'))
-
-        model_copy = deepcopy(self.ViT)
-        model_copy.ViT[1] = torch.nn.Identity()
-
-        feature_extractor = torch.nn.Sequential(
-            model_copy,
-            torch.nn.Flatten()
-        )
-
-        features = feature_extractor(images)
-
-        score = drift_detector(features)
-        p_val = drift_detector.compute_p_value(features)
-        print(f'Drift score {score:.2f} and p-value {p_val:.2f}')
 
 def main():
     # Training settings
@@ -49,9 +32,27 @@ def main():
     filepath_model = glob(os.path.join('models/trained_models', args.load_timestamp, '*.ckpt'))
 
     model = ViT.load_from_checkpoint(checkpoint_path=filepath_model[0])
-    trainer = Trainer(callbacks=[DriftDetection()])
+    trainer = Trainer()
     predictions = trainer.predict(model, predictloader)
     print(predictions)
+
+    drift_detector = torch.load(os.path.join('models/trained_models', args.load_timestamp, '*.pt'))
+
+    model_copy = deepcopy(model)
+    model_copy.ViT[1] = torch.nn.Identity()
+
+    feature_extractor = torch.nn.Sequential(
+        model_copy,
+        torch.nn.Flatten()
+    )
+
+    images = next(iter(predictloader))[0]
+
+    features = feature_extractor(images)
+
+    score = drift_detector(features)
+    p_val = drift_detector.compute_p_value(features)
+    print(f'Drift score {score:.2f} and p-value {p_val:.2f}')
 
 
 if __name__ == "__main__":
