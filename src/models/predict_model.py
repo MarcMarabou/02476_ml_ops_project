95b176a34d4aa5986b0e49352a0ae465084d60e3
Marc Bøg <marc@boeg.io>
2022-01-20 11:52:01 +0100

    test script for deployed model and prediction script with drift check

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -8,6 +8,7 @@ from glob import glob
 import gcsfs
 import matplotlib.pyplot as plt
 import torch
+import torchvision.transforms as T
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import Callback
 
@@ -20,7 +21,7 @@ def main():
     # Training settings
     args = get_args()
 
-    # Load the training data
+    # Load the training data # transforms=T.Compose([T.ToTensor(), T.GaussianBlur((127,127), (10, 10))])
     predict_set = FlowerDataset(args.data_path, "224x224", "test")
     predictloader = torch.utils.data.DataLoader(
         predict_set,
@@ -39,7 +40,7 @@ def main():
     print(predictions)
 
     drift_detector = torch.load(
-        os.path.join("models/trained_models", args.load_timestamp, "*.pt")
+        os.path.join("models/trained_models", args.load_timestamp, "drift_detector.pt")
     )
 
     model_copy = deepcopy(model)
